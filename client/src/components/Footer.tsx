5e229cd4ffd9df1ff457ba8fd6e59988077bcbc2
kenwoo9y <kenwoo9y@gmail.com>
2024-07-07 22:49:44 +0900

    Add Header and Footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-blue-500 text-white p-4 fixed bottom-0 w-full text-center">
+            <p>&copy; 2024 kenwoo9y</p>
+        </footer>
+    );
+}
+
+export default Footer;
