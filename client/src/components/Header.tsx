b199aa909c1d7eaa7ab1c34ca737892d798cc6ef
kenwoo9y <kenwoo9y@gmail.com>
2024-07-09 23:49:04 +0900

    Add Sign out button to the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HamburgerMenu from "./HamburgerMenu";
+import { LogIn } from "lucide-react";
 
 interface HeaderProps {
     toggleSideMenu: () => void;
@@ -7,9 +8,15 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ toggleSideMenu }) => {
     return (
-        <header className="bg-blue-500 text-white p-4 flex items-center z-10 fixed w-full">
-            <HamburgerMenu toggleSideMenu={toggleSideMenu} />
-            <h1 className="text-2xl">ToDo App</h1>
+        <header className="bg-blue-500 text-white p-4 flex items-center justify-between z-10 fixed w-full">
+            <div className="flex items-center">
+                <HamburgerMenu toggleSideMenu={toggleSideMenu} />
+                <h1 className="text-2xl">ToDo App</h1>
+            </div>
+            <div className="flex items-center ml-auto">
+                <LogIn className="w-6 h-6 cursor-pointer" />
+                <span className="ml-2 cursor-pointer">Sign Out</span>
+            </div>
         </header>
     );
 }
