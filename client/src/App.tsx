dd188f9cec8e690e0041f2c185c9a98d1fc05877
kenwoo9y <kenwoo9y@gmail.com>
2024-07-20 20:50:10 +0900

    Fix Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import './index.css'
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import SideMenu from "./components/SideMenu";
 import AppRouter from "./router/Router";
 
 const App: React.FC = () => {
-    const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-
-    const toggleSideMenu = () => {
-      setIsSideMenuOpen(!isSideMenuOpen);
-    };
-
-    const closeSideMenu = () => {
-      setIsSideMenuOpen(false);
-    };
-
-    return (
-      <div className="flex flex-col min-h-screen">
-          <Header toggleSideMenu={toggleSideMenu} />
-          <SideMenu isOpen={isSideMenuOpen} closeSideMenu={closeSideMenu} />
-          <main className={`flex-grow pt-16 p-4 transition-all duration-300 ease-in-out ${isSideMenuOpen ? 'ml-64' : ''}`}>
-            <div className="container mx-auto p-4 bg-white">
-              <AppRouter />
-            </div>
-          </main>
-          <Footer />
-      </div>
-    );
+    return <AppRouter />;
 }
 
 export default App;
