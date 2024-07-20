131d8e7858ba1866fe52f3ab4efc42b912df07a7
kenwoo9y <kenwoo9y@gmail.com>
2024-07-20 20:51:29 +0900

    Changed to transition to the root when AppName is clicked

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import HamburgerMenu from "./HamburgerMenu";
 import { LogIn } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
     toggleSideMenu: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ toggleSideMenu }) => {
+    const navigate = useNavigate();
+
+    const handleAppNameClick = () => {
+        navigate('/');
+    };
+
     return (
         <header className="bg-blue-500 text-white p-4 flex items-center justify-between z-10 fixed w-full">
             <div className="flex items-center">
                 <HamburgerMenu toggleSideMenu={toggleSideMenu} />
-                <h1 className="text-2xl">ToDo App</h1>
+                <h1 className="text-2xl" onClick={() => handleAppNameClick()}>
+                    ToDo App
+                </h1>
             </div>
             <div className="flex items-center ml-auto">
                 <LogIn className="w-6 h-6 cursor-pointer" />
