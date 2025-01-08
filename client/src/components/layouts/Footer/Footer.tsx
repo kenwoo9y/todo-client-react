7ff3f33f680373c452ea90cb1a43a03adea32243
kenwoo9y <kenwoo9y@gmail.com>
2025-01-08 23:53:30 +0900

    move footer

--- client/src/components/layouts/Footer/Footer.tsx
+++ client/src/components/layouts/Footer/Footer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Footer: React.FC = () => {
+export const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 w-full bg-blue-500 p-4 text-center text-white">
       <p>&copy; 2024 kenwoo9y</p>
     </footer>
   );
 };
-
-export default Footer;
