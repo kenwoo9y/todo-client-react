74a550ccf948c0a2abba83e00e77d0e5fc4a2873
kenwoo9y <kenwoo9y@gmail.com>
2025-01-09 23:56:39 +0900

    fix header

---
+++ client/src/components/layouts/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HamburgerMenu } from '../HamburgerMenu';
+import { LogIn } from 'lucide-react';
+
+export const Header: React.FC = () => {
+  return (
+    <header className="fixed z-10 flex w-full items-center justify-between bg-blue-500 p-4 text-white">
+      <HamburgerMenu />
+      <div className="flex items-center">
+        <h1 className="text-2xl cursor-pointer">ToDo App</h1>
+      </div>
+      <div className="ml-auto flex items-center">
+        <LogIn className="size-6 cursor-pointer" />
+        <span className="ml-2 cursor-pointer">Sign Out</span>
+      </div>
+    </header>
+  );
+};
