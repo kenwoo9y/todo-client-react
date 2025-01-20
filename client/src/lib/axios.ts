26c4e8087da6400343bf9c316378204b06c323d0
kenwoo9y <kenwoo9y@gmail.com>
2025-01-20 22:37:44 +0900

    define apiClient

---
+++ client/src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { env } from '../config/env';
+
+export const apiClient = axios.create({
+  baseURL: env.apiUrl,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
