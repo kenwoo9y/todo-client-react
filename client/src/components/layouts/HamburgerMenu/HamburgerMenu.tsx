28a26726d42347d71e958cd345c0b3889caf453d
kenwoo9y <kenwoo9y@gmail.com>
2025-01-25 18:43:09 +0900

    click on a Menu item to move to another page

--- client/src/components/layouts/HamburgerMenu/HamburgerMenu.tsx
+++ client/src/components/layouts/HamburgerMenu/HamburgerMenu.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Home, MessagesSquare } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 import './HamburgerMenu.css';
 
 export const HamburgerMenu: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Menu>
+    <Menu isOpen={isOpen} onStateChange={(state) => setIsOpen(state.isOpen)}>
       <ul>
-        <li className="menu-item flex cursor-pointer">
-          <Home className="mr-2 size-6" />
-          Home
+        <li className="menu-item">
+          <Link to="/" className="flex cursor-pointer" onClick={handleClose}>
+            <Home className="mr-2 size-6" />
+            Home
+          </Link>
         </li>
-        <li className="menu-item flex cursor-pointer">
-          <MessagesSquare className="mr-2 size-6" />
-          About
+        <li className="menu-item">
+          <Link
+            to="/about"
+            className="flex cursor-pointer"
+            onClick={handleClose}
+          >
+            <MessagesSquare className="mr-2 size-6" />
+            About
+          </Link>
         </li>
       </ul>
     </Menu>
