3c587080ed033ef2adc1fbb9b5f7cfc25fc9e01c
kenwoo9y <kenwoo9y@gmail.com>
2025-02-14 22:33:42 +0900

    update test setting

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
